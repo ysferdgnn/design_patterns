f99a23d1c2fdc96411f3a38908889ff003eea4c8
ysferdgnn <ysferdgnn@users.noreply.github.com>
2022-08-13 21:00:32 +0300

    add lombok and change connection implementations with lombok

--- src/main/java/creational/factory/factorymethod/solution1/PostgreConnection.java
+++ src/main/java/creational/factory/factorymethod/solution1/PostgreConnection.java
@@ -1,5 +1,10 @@
 package creational.factory.factorymethod.solution1;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class PostgreConnection implements Connection{
     private final String databaseName;
     private final String port;
@@ -7,34 +12,12 @@ public class PostgreConnection implements Connection{
     private final String password;
 
 
-    public PostgreConnection(String databaseName, String port, String username, String password) {
-        this.databaseName = databaseName;
-        this.port = port;
-        this.username = username;
-        this.password = password;
-
-    }
-
     @Override
     public void connect() {
         System.out.printf("connecting database: %s, port: %s%n",databaseName,port);
     }
 
-    public String getDatabaseName() {
-        return databaseName;
-    }
 
-    public String getPort() {
-        return port;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 
 
 }
